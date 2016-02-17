bff9cd9151a30e5d3b3c1149f67cd9b35077317b
bambangadrian <bambang.adrian@gmail.com>
2016-02-17 18:57:06 +0700

    fix index page

---
+++ src/app/Views/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+
+<h1>Recruitment Test - Solutions</h1>
+<ol>
+    <li><a href="?m=BankInterest">Bank Interest Calculation (Bunga Bank)</a></li>
+    <li><a href="?m=RecursiveMultiplication">Recursion Multiplication (Perkalian Rekursif)</a></li>
+    <li><a href="?m=EncodingDecoding">Encoding/Decoding</a></li>
+    <li><a href="?m=ShortestPathProblem">Shortest Path Problem (Using Djikstra Algorithm - <i>Doubly Linked
+        List</i>)</a></li>
+    <li><a href="?m=SinusSpatialCalculation">Sinus Graph Spatial Calculation (Menghitung Luas Fungsi Sin)</a>
+    </li>
+    <li><a href="?m=CurrencyConversion">Currency Rate Conversion (Konversi Mata Uang)</a></li>
+    <li><a href="?m=RotatingEncryption">Rotating Encryption</a></li>
+</ol>
+
+<h4><a href="https://github.com/bambangadrian/recruitmentTest/blob/master/README.md">README</a></h4>
+</body>
+</html>
